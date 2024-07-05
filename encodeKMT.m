58eebad7a213c565026b747ae1340cdda1d0c9ca
tang <86227275+87sweet@users.noreply.github.com>
2024-07-05 22:39:15 +0800

    Add files via upload

--- encodeKMT.m
+++ encodeKMT.m
@@ -1,3 +1,14 @@
+% function chromKMT=encodeKMT(C_KMT)
+% d=size(C_KMT,2); 
+% Z=zeros(NIND,d);
+% Z(1,:)=rand(1,d);
+% for i=2:NIND
+%     Z(i,:)=4* Z(i-1,:).*(1-Z(i-1,:));
+% end
+% chromKMT=zeros(NIND,d);
+% for i=1:NIND
+%     chromKMT(i,:)=round(1+Z(i,:)*(d-1));
+% end
 
 function chromKMT=encodeKMT(C_KMT)
 kinds=size(C_KMT,2);    
--- encodeOMT.m
+++ encodeOMT.m
@@ -1,4 +1,14 @@
-
+% function chromKMT=encodeOMT(C_KMT)
+% d=size(E1_OMT,2); 
+% Z=zeros(NINDO,d);
+% Z(1,:)=rand(1,d);
+% for i=2:NIND
+%     Z(i,:)=4* Z(i-1,:).*(1-Z(i-1,:));
+% end
+% chromOMT=zeros(NIND,d);
+% for i=1:NIND
+%     chromOMT(i,:)=round(1+Z(i,:)*(d-1));
+% end
 function chromOMT=encodeOMT(E1)
 kinds=size(E1,2);    
 nums=size(E1,1); 
